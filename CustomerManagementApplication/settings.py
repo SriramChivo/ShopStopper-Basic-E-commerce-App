fd4084858fd0fcd640fbbfcb8aa111fa8728bb6e
Sriram Sundharraman <61426005+SriramChivo@users.noreply.github.com>
2020-07-26 20:11:06 +0530

    Update settings.py

--- CustomerManagementApplication/settings.py
+++ CustomerManagementApplication/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n60kmt0tp6dy#-esl7cv(h8)9zg+vetc=@er1c(uc^qbsq+c&9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["shopstopper.herokuapp.com", "127.0.0.1"]
 
