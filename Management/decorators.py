62e190bd69ace94e71e06eaacf63c619a7d89372
Sriram <sriramchivo@07@gmail.com>
2020-07-28 01:46:37 +0530

    changed things

--- Management/decorators.py
+++ Management/decorators.py
@@ -5,13 +5,11 @@ from django.urls import reverse
 def AllowedUsers(allowedroles=[]):
     def decorator(view_func):
         def wrapper_func(request, *args, **kwargs):
-            group = None
             grp = request.user.groups.all()
             for i in grp:
                 if(i.name in allowedroles):
                     return view_func(request, *args, **kwargs)
-                else:
-                    print("not working")
-                    return HttpResponseRedirect(reverse("Unauth"))
+            print("not working")
+            return HttpResponseRedirect(reverse("Unauth"))
         return(wrapper_func)
     return(decorator)
